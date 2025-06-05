becad5e943fe0a68020dce37c6cc8283cc280607
manuelgcsousa <manuelgcsousa@gmail.com>
2025-06-05 22:55:27 +0100

    [wezterm] conditional settings + keymaps rework

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local is_linux = wezterm.target_triple:find("linux") ~= nil
 
-config.disable_default_key_bindings = true
+--
+-- ASPECT & COLORS
+--
 
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
-
-config.keys = {
-  { key = "r", mods = "SUPER", action = wezterm.action.ReloadConfiguration, },
-  { key = "v", mods = "SUPER", action = wezterm.action.PasteFrom("Clipboard") },
-  { key = "+", mods = "SUPER", action = wezterm.action.IncreaseFontSize, },
-  { key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize, },
-  { key = "0", mods = "SUPER", action = wezterm.action.ResetFontSize },
-  { key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true })},
-  { key = "q", mods = "CMD", action = wezterm.action.QuitApplication },
-}
+config.enable_tab_bar = false
 
 config.window_padding = {
   left = 2,
@@ -58,10 +50,40 @@ config.font = wezterm.font(
     harfbuzz_features = { "calt=0" }
   }
 )
-config.font_size = 16
+config.font_size = is_linux and 12 or 16
 config.line_height = 1.2
 config.front_end = "WebGpu"
 
-config.enable_tab_bar = false
+
+--
+-- MAPPINGS
+--
+
+config.disable_default_key_bindings = true
+
+if is_linux then
+  config.keys = {
+    { key = "r", mods = "CTRL|SHIFT", action = wezterm.action.ReloadConfiguration },
+    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+    { key = "+", mods = "CTRL",       action = wezterm.action.IncreaseFontSize },
+    { key = "-", mods = "CTRL",       action = wezterm.action.DecreaseFontSize },
+    { key = "0", mods = "CTRL",       action = wezterm.action.ResetFontSize },
+  }
+else
+  config.send_composed_key_when_left_alt_is_pressed = true
+  config.send_composed_key_when_right_alt_is_pressed = true
+
+  config.keys = {
+    { key = "r", mods = "SUPER", action = wezterm.action.ReloadConfiguration },
+    { key = "c", mods = "SUPER", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+    { key = "v", mods = "SUPER", action = wezterm.action.PasteFrom("Clipboard") },
+    { key = "+", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
+    { key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize },
+    { key = "0", mods = "SUPER", action = wezterm.action.ResetFontSize },
+    { key = "w", mods = "CMD",   action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+    { key = "q", mods = "CMD",   action = wezterm.action.QuitApplication },
+  }
+end
 
 return config

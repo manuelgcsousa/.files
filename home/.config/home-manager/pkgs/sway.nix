544dfef7babe4231ca51f18e342840b48c667e24
manuelgcsousa <manuelgcsousa@gmail.com>
2025-12-06 16:26:47 +0000

    feat: new waybar + small sway tweaks + simplify dunst

--- home/.config/home-manager/pkgs/sway.nix
+++ home/.config/home-manager/pkgs/sway.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 with pkgs; [
-  wl-clipboard   # wayland clipboard tools
-  # sway         # window manager
-  # swaylock     # screen locker
-  # swaybg       # wallpaper tool
-  waybar         # status bar
-  grim           # take screenshot
-  slurp          # select region on screenshot
-  fuzzel         # launcher
-  nautilus       # gnome GUI file manager
-  nwg-displays   # manual output display manager
-  hyprpicker     # color picker
+  # sway          # window manager
+  # swaylock      # screen locker
+  # swaybg        # wallpaper tool
+  waybar        # status bar
+  wl-clipboard  # wayland clipboard tools
+  grim          # take screenshot
+  slurp         # select region on screenshot
+  fuzzel        # launcher
+  pcmanfm       # GUI file manager
+  nwg-displays  # manual output display manager
+  hyprpicker    # color picker
 ]
